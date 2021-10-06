5a3ea822bcfa263579de32f485ba0c97ae61f3d1
Joanna Wrzesinska <joanna.n.wrzesinska@gsk.com>
2021-10-06 12:58:50 +0200

    using state, deleteTask, toggleReminder

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -27,11 +27,28 @@ function App() {
 
   //delete task
 
-  const deleteTask = () => {};
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  //toggle reminder
+
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
+
   return (
     <div className='App container'>
       <Header />
-      <Tasks tasks={tasks} />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "no tasks"
+      )}
     </div>
   );
 }
